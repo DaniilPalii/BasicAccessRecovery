85edd36e61cc00d18ef218a19346d30936bbddec
DaniilPalii <palii.daniil.o@gmail.com>
2020-05-03 14:32:41 +0200

    Allow input login and allowed symbols

--- BasicAccessRecovery/Program.cs
+++ BasicAccessRecovery/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WordListGenerator;
@@ -9,19 +10,30 @@ namespace BasicAccessRecovery
     {
         public static async Task Main(string[] args)
         {
-            var uri = new Uri(ConsoleHelper.ReadLine("Uri to recovery access"));
+            var uri = new Uri(ConsoleHelper.ReadString("Uri to recovery access"));
+            
+            var wordsCreator = new WordsCreator();
+            var allowedSymbolsInput = ConsoleHelper.ReadString(
+                "Allowed credentials symbols "
+                    + "(don't use separators) "
+                    + $"(leave empty to use default \"{wordsCreator.AllowedSymbols}\")");
+            if (allowedSymbolsInput != null)
+                wordsCreator.AllowedSymbols = allowedSymbolsInput.ToArray();
+            
             var minWordLength = ConsoleHelper.ReadInt("Input minimal word length (default is 1)") ?? 1;
             var maxWordLength = ConsoleHelper.ReadInt("Input maximal word length (default is 12)") ?? 12;
-
-            var httpClient = new HttpClient();
-            var wordsCreator = new WordsCreator();
             
+            var loginInput = ConsoleHelper.ReadString("Login (leave empty to try recover)");
+
             Console.WriteLine("Trying to recovery access...");
 
-            foreach (
-                var (login, password)
-                in wordsCreator.CreateAllWords(minWordLength, maxWordLength)
-                    .CrossJoin(wordsCreator.CreateAllWords(minWordLength, maxWordLength), (l, p) => (l, p)))
+            var httpClient = new HttpClient();
+            var credentials = loginInput != null
+                ? wordsCreator.CreateAllWords(minWordLength, maxWordLength).Select(p => (loginInput, p))
+                : wordsCreator.CreateAllWords(minWordLength, maxWordLength)
+                    .CrossJoin(wordsCreator.CreateAllWords(minWordLength, maxWordLength), (l, p) => (l, p));
+
+            foreach (var (login, password) in credentials)
             {
                 var uriWithCredentials = new UriBuilder(uri) { UserName = login, Password = password }.Uri;
                 var gettingTask = httpClient.GetAsync(uriWithCredentials);
